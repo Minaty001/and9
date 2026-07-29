21d6fabb2a9b3f6ca72575285dec51730003f315
root <root@localhost.localdomain>
2026-07-29 23:55:10 +0400

    fix build.sh: auto-create and activate venv to avoid externally-managed-environment error

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# Use virtual environment if it exists, otherwise create one
+if [ ! -d "venv" ]; then
+    python3 -m venv venv
+fi
+source venv/bin/activate
+
 pip install --upgrade pip
 pip install -r requirements.txt
 
